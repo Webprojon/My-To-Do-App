60ca1baf26d1041fea9bc2b3e3cd52e1c75f03c6
Tokhirjon <tokhirjon0107@gmail.com>
2023-03-13 10:14:09 +0100

    fixed some error

--- js/app.js
+++ js/app.js
@@ -1,57 +1,58 @@
-  // Selectors ************************
-  const addBtn = document.querySelector(".add-lists");
-  const lists = document.querySelector("#lists");
-  const title = document.querySelector("h1");
-  const inputTypingValue = document.querySelector("#todolist");
+// Selectors ************************
+const addBtn = document.querySelector(".add-lists");
+const lists = document.querySelector("#lists");
+const title = document.querySelector("h1");
+const inputTypingValue = document.querySelector("#todolist");
 
-  // Event Listers **********************
-  addBtn.addEventListener("click", addList);
-  inputTypingValue.addEventListener("input", disableBtn);
+// Event Listers **********************
+addBtn.addEventListener("click", addList);
+inputTypingValue.addEventListener("input", disableBtn);
 
-  // Functions **************************
-  function disableBtn(e) {
-    let eValue = e.target.value;
-    if (eValue.length >= 10) {
-      addBtn.removeAttribute("disabled");
-    } else {
-      addBtn.setAttribute("disabled", "disabled");
-    }
+// Functions **************************
+function disableBtn(e) {
+  let eValue = e.target.value;
+  if (eValue.length >= 10) {
+    addBtn.removeAttribute("disabled");
+  } else {
+    addBtn.setAttribute("disabled", "disabled");
   }
+}
 
-  function addList() {
-    const inputValue = document.querySelector("#todolist").value;
-    const liElement = document.createElement("li");
+function addList(e) {
+  e.preventDefault();
+  const inputValue = document.querySelector("#todolist").value;
+  const liElement = document.createElement("li");
 
-    liElement.innerText = `${inputValue}`;
-    lists.appendChild(liElement);
+  liElement.innerText = `${inputValue}`;
+  lists.appendChild(liElement);
 
-    // List Buttons *******************************
-    const parentBtns = document.createElement("div");
-    parentBtns.classList.add("btns");
+  // List Buttons *******************************
+  const parentBtns = document.createElement("div");
+  parentBtns.classList.add("btns");
 
-    // Creating Button ***************************
-    const closeBtn = document.createElement("button");
-    closeBtn.classList.add("close");
-    closeBtn.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
-    parentBtns.appendChild(closeBtn);
+  // Creating Button ***************************
+  const closeBtn = document.createElement("button");
+  closeBtn.classList.add("close");
+  closeBtn.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  parentBtns.appendChild(closeBtn);
 
-    // Creating Button ****************************
-    const completedBtn = document.createElement("button");
-    completedBtn.classList.add("completed");
-    completedBtn.innerHTML = `<i class="fa-solid fa-check"></i>`;
-    parentBtns.appendChild(completedBtn);
+  // Creating Button ****************************
+  const completedBtn = document.createElement("button");
+  completedBtn.classList.add("completed");
+  completedBtn.innerHTML = `<i class="fa-solid fa-check"></i>`;
+  parentBtns.appendChild(completedBtn);
 
-    // Parent Childing ***************************
-    liElement.appendChild(parentBtns);
+  // Parent Childing ***************************
+  liElement.appendChild(parentBtns);
 
-    closeList(closeBtn, completedBtn, liElement);
-    // List Buttons *******************************
+  closeList(closeBtn, completedBtn, liElement);
+  // List Buttons *******************************
 
-    title.style.display = "none";
+  title.style.display = "none";
 
-    // removeLastChild Button ********************
-    removeChildren(lists, title);
-  }
+  // removeLastChild Button ********************
+  removeChildren(lists, title);
+}
 
 // removing LastChild *************************
 function removeChildren(lists, title) {
